46c87480e1ad0004665f9eaf95749c02654dcb7a
Nile Kelly <nilekell@gmail.com>
2023-06-03 13:46:34 +0100

    making upload bloc available throughout the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:receiptcamp/data/repositories/database_repository.dart';
+import 'package:receiptcamp/logic/blocs/upload/upload_bloc.dart';
 import 'package:receiptcamp/presentation/screens/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DatabaseRepository.instance.init();
-  runApp(const MyApp());
+  runApp(BlocProvider<UploadBloc>(
+      create: (BuildContext context) => UploadBloc()..add(UploadInitialEvent()),
+      child: const MyApp(),
+    ));
 }
 
 class MyApp extends StatelessWidget {
