cd2b84c00c56daa8a77af66514d4c9e01668299a
Nile Kelly <nilekell@gmail.com>
2023-06-03 16:03:41 +0100

    added text recognition util

---
+++ lib/data/utils/text_recognition.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/services.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+
+class TextRecognitionService {
+  static List<String> _commonReceiptWords = [];
+
+  Future<List<String>> extractKeywordsFromImage(String imagePath) async {
+    try {
+      final scannedTextList = await scanImageForText(imagePath);
+      final receiptKeyWords = await extractKeywords(scannedTextList);
+
+      return receiptKeyWords;
+    } on Exception catch (e) {
+      print('Error in extractKeywordsFromImage: $e');
+      return [];
+    }
+  }
+
+  static Future<List> scanImageForText(String imagePath) async {
+    // Uses Google ML Kit Vision
+    try {
+      final inputImage = InputImage.fromFilePath(imagePath);
+      final textRecognizer =
+          TextRecognizer(script: TextRecognitionScript.latin);
+      final RecognizedText recognizedText =
+          await textRecognizer.processImage(inputImage);
+      String scannedText = recognizedText.text;
+      List scannedTextList = [];
+      for (TextBlock block in recognizedText.blocks) {
+        for (TextLine line in block.lines) {
+          for (TextElement element in line.elements) {
+            scannedText = element.text.toLowerCase();
+            scannedTextList.add(scannedText);
+          }
+        }
+      }
+
+      return scannedTextList;
+    } on Exception catch (e) {
+      print('Error in scanImageForText: $e');
+      return [];
+    }
+  }
+
+  Future<List<String>> extractKeywords(List scannedOCRText) async {
+    try {
+      String scannedOCRTextList = scannedOCRText.join(' ');
+      // Define the regular expression pattern for word characters.
+      final pattern = RegExp(r'\w+');
+      // Use the pattern to match all the words in the text.
+      final matches = pattern.allMatches(scannedOCRTextList);
+      // Convert the matches to a list of strings.
+      final words = matches.map((match) => match.group(0)!).toList();
+      // Use the _commonReceiptWords list as stop words.
+      await _loadCommonReceiptWords();
+      // Filter out any stop words and return the remaining words.
+      final keywords = words
+          .where((word) => !_commonReceiptWords.contains(word.toLowerCase()))
+          .toList();
+      return keywords;
+    } on Exception catch (e) {
+      print('Error in extractKeywords: $e');
+      return [];
+    }
+  }
+
+  static Future<void> _loadCommonReceiptWords() async {
+    try {
+      if (_commonReceiptWords.isEmpty) {
+        String content =
+            await rootBundle.loadString('assets/common_receipt_words.txt');
+        _commonReceiptWords =
+            content.split('\n').map((word) => word.trim()).toList();
+      }
+    } on Exception catch (e) {
+      print('Error in _loadCommonReceiptWords: $e');
+    }
+  }
+}
