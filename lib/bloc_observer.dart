4c462edb999301194cf1d28f8e9c9f1dcc709618
Nile Kelly <nilekell@gmail.com>
2023-06-06 18:17:59 +0100

    removing bloc observe onChange()

--- lib/bloc_observer.dart
+++ lib/bloc_observer.dart
@@ -1,12 +1,15 @@
 import 'package:bloc/bloc.dart';
 
 class AppBlocObserver extends BlocObserver {
-
+  /*
+  commenting onChange method out as it outputs what onTransition does, except without the event.
+  Using both methods makes terminal output when running unnecessarily verbose
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
     print('${bloc.runtimeType} $change');
   }
+  */
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
