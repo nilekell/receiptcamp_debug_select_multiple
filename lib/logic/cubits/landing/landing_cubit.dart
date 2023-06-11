31bf822de85b5c63a8965898ce774f97fa69f821
Nile Kelly <nilekell@gmail.com>
2023-06-11 13:57:27 +0100

    emitting bottom nav bar index based on current value

--- lib/logic/cubits/landing/landing_cubit.dart
+++ lib/logic/cubits/landing/landing_cubit.dart
@@ -4,13 +4,6 @@ class LandingCubit extends Cubit<int> {
   LandingCubit() : super(0);
 
   updateIndex(int value) {
-    switch (value) {
-      case 0:
-        emit(state + 1);
-      case 1:
-        emit(state - 1);
-      default:
-        emit(-1);
-    }
+    emit(value);
   }
-  }
+}
