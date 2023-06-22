3358000f43c547d6d0d42efa251820e96184c1f3
Nile Kelly <nilekell@gmail.com>
2023-06-22 19:37:12 +0100

    using single instance of UploadBloc across FileExplorer and upload bottom sheet

--- lib/presentation/ui/file_navigator/upload_sheet.dart
+++ lib/presentation/ui/file_navigator/upload_sheet.dart
@@ -1,40 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:receiptcamp/logic/blocs/upload/upload_bloc.dart';
-import 'package:receiptcamp/presentation/ui/file_navigator/create_folder_dialog.dart';
+import 'package:receiptcamp/presentation/ui/file_navigator/folder_dialog.dart';
 
-void showUploadOptions(BuildContext context) {
+void showUploadOptions(BuildContext context, UploadBloc uploadBloc) {
   showModalBottomSheet(
-      context: context,
-      builder: (context) => BlocProvider(
-                create: (BuildContext context) => UploadBloc()..add(UploadInitialEvent()),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ListTile(
-                  leading: const Icon(Icons.photo_library),
-                  title: const Text('Choose from gallery'),
-                  onTap: () {
-                    UploadBloc().add(UploadTapEvent());
-                    Navigator.of(context).pop();
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(Icons.camera),
-                  title: const Text('Take a photo'),
-                  onTap: () {
-                    UploadBloc().add(CameraTapEvent());
-                    Navigator.of(context).pop();
-                  },
-                ),
-                ListTile(
-                  leading: const Icon(Icons.folder),
-                  title: const Text('Create folder'),
-                  onTap: () async {
-                    showFolderDialog(context);
-                  },
-                ),
-              ],
-            ),
-          ));
+    context: context,
+    builder: (bottomSheetContext) => Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ListTile(
+          leading: const Icon(Icons.photo_library),
+          title: const Text('Choose from gallery'),
+          onTap: () {
+            Navigator.of(bottomSheetContext).pop();
+            uploadBloc.add(UploadTapEvent());
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.camera),
+          title: const Text('Take a photo'),
+          onTap: () {
+            Navigator.of(bottomSheetContext).pop();
+            uploadBloc.add(CameraTapEvent());
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.folder),
+          title: const Text('New folder'),
+          onTap: () {
+            // closing bottom sheet
+            Navigator.of(bottomSheetContext).pop();
+            showFolderDialog(bottomSheetContext, uploadBloc);
+          },
+        ),
+      ],
+    ),
+  );
 }
