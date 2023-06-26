14551e98cf1436c451e0bcb087f5792bd5deda47
Nile Kelly <nilekell@gmail.com>
2023-06-26 17:23:50 +0100

    opening receipt renaming dialog from receipt bottom sheet

--- lib/presentation/ui/file_navigator/receipt/receipt_sheet.dart
+++ lib/presentation/ui/file_navigator/receipt/receipt_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:receiptcamp/logic/cubits/file_edit/file_editing_cubit.dart';
 import 'package:receiptcamp/models/receipt.dart';
+import 'package:receiptcamp/presentation/ui/file_navigator/receipt/rename_receipt_dialog.dart';
 
 void showReceiptOptions(BuildContext context, FileEditingCubit fileEditingCubit, Receipt receipt) {
   showModalBottomSheet(
@@ -12,7 +13,10 @@ void showReceiptOptions(BuildContext context, FileEditingCubit fileEditingCubit,
           leading: const Icon(Icons.edit),
           title: const Text('Rename'),
           onTap: () {
-            // show rename receipt dialog
+            // closing bottom sheet
+            Navigator.of(bottomSheetContext).pop();
+            // opening rename receipt dialog
+            showRenameReceiptDialog(bottomSheetContext, fileEditingCubit, receipt);
           },
         ),
         ListTile(
