3d860d3cdd0631d31561e5419ea4208181bcd163
Nile Kelly <nilekell@gmail.com>
2023-06-27 14:06:43 +0100

    adding initial folder edit bottom sheet

---
+++ lib/presentation/ui/file_navigator/folder/folder_sheet.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:receiptcamp/logic/cubits/file_edit/file_editing_cubit.dart';
+import 'package:receiptcamp/models/folder.dart';
+import 'package:receiptcamp/presentation/ui/file_navigator/folder/rename_folder_dialog.dart';
+void showFolderOptions(BuildContext context, FileEditingCubit fileEditingCubit, Folder folder) {
+  showModalBottomSheet(
+    context: context,
+    builder: (bottomSheetContext) => Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ListTile(
+          leading: const Icon(Icons.edit),
+          title: const Text('Rename'),
+          onTap: () {
+            // closing bottom sheet
+            Navigator.of(bottomSheetContext).pop();
+            // opening rename folder dialog
+            showRenameFolderDialog(bottomSheetContext, fileEditingCubit, folder);
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.drive_file_move),
+          title: const Text('Move'),
+          onTap: () {
+            // closing bottom sheet
+            Navigator.of(bottomSheetContext).pop();
+            // show move folder dialog
+            showRenameFolderDialog(context, fileEditingCubit, folder);
+          },
+        ),
+        ListTile(
+          leading: const Icon(Icons.delete),
+          title: const Text('Delete'),
+          onTap: () {
+            fileEditingCubit.deleteFolder(folder.id);
+          },
+        ),
+      ],
+    ),
+  );
+}
