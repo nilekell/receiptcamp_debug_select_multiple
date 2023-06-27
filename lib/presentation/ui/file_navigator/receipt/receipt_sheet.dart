954ed0e82fc34447738ad064e9f7275540de0ceb
Nile Kelly <nilekell@gmail.com>
2023-06-27 21:57:15 +0100

    invoking showMoveReceiptDialog() on receipt bottom sheet

--- lib/presentation/ui/file_navigator/receipt/receipt_sheet.dart
+++ lib/presentation/ui/file_navigator/receipt/receipt_sheet.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:receiptcamp/logic/cubits/file_edit/file_editing_cubit.dart';
 import 'package:receiptcamp/models/receipt.dart';
 import 'package:receiptcamp/presentation/ui/file_navigator/receipt/delete_receipt_confirmation_dialog.dart';
+import 'package:receiptcamp/presentation/ui/file_navigator/receipt/move_receipt_dialog.dart';
 import 'package:receiptcamp/presentation/ui/file_navigator/receipt/rename_receipt_dialog.dart';
 
 void showReceiptOptions(BuildContext context, FileEditingCubit fileEditingCubit, Receipt receipt) {
@@ -24,7 +25,9 @@ void showReceiptOptions(BuildContext context, FileEditingCubit fileEditingCubit,
           leading: const Icon(Icons.drive_file_move),
           title: const Text('Move'),
           onTap: () {
+            Navigator.of(bottomSheetContext).pop();
             // show move receipt dialog
+            showMoveReceiptDialog(context, fileEditingCubit, receipt);
           },
         ),
         ListTile(
