b67fe060d9f93a5ab8e53e5ec530250d151c93e2
Nile Kelly <nilekell@gmail.com>
2023-07-06 13:06:42 +0100

    adding currentFolderId as attribute of UploadOptionsBottomSheet so FileSystemCubit.upload...() methods can upload object to currently displayed folder

--- lib/presentation/ui/file_navigator/upload_sheet.dart
+++ lib/presentation/ui/file_navigator/upload_sheet.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:receiptcamp/logic/cubits/file_system/file_system_cubit.dart';
+import 'package:receiptcamp/models/folder.dart';
 import 'package:receiptcamp/presentation/ui/file_navigator/folder/create_folder_dialog.dart';
 
-void showUploadOptions(BuildContext context, FileSystemCubit fileSystemCubit) {
+void showUploadOptions(BuildContext context, FileSystemCubit fileSystemCubit, Folder currentFolder) {
   showModalBottomSheet(
     context: context,
     builder: (bottomSheetContext) {
       return BlocProvider.value(
         value: fileSystemCubit,
-        child: const UploadOptionsBottomSheet(),
+        child: UploadOptionsBottomSheet(currentFolderId: currentFolder.id,),
       );
     },
   );
 }
 
 class UploadOptionsBottomSheet extends StatelessWidget {
-  const UploadOptionsBottomSheet({super.key});
+  final String currentFolderId;
+
+  const UploadOptionsBottomSheet({super.key, required this.currentFolderId});
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +30,7 @@ class UploadOptionsBottomSheet extends StatelessWidget {
           leading: const Icon(Icons.photo_library),
           title: const Text('Choose from gallery'),
           onTap: () {
-            context.read<FileSystemCubit>().uploadReceipt();
+            context.read<FileSystemCubit>().uploadReceipt(currentFolderId);
             // closing bottom sheet
             Navigator.of(context).pop();
           },
@@ -36,7 +39,7 @@ class UploadOptionsBottomSheet extends StatelessWidget {
           leading: const Icon(Icons.camera),
           title: const Text('Take a photo'),
           onTap: () {
-            context.read<FileSystemCubit>().uploadReceiptFromCamera();
+            context.read<FileSystemCubit>().uploadReceiptFromCamera(currentFolderId);
             // closing bottom sheet
             Navigator.of(context).pop();
           },
