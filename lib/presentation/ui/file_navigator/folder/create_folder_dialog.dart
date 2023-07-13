f2189f78867a2655ced948b1ccd4e1a9ce516784
Nile Kelly <nilekell@gmail.com>
2023-07-13 21:43:07 +0100

    passing current folder to create folder process

--- lib/presentation/ui/file_navigator/folder/create_folder_dialog.dart
+++ lib/presentation/ui/file_navigator/folder/create_folder_dialog.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:receiptcamp/logic/cubits/folder_view/folder_view_cubit.dart';
+import 'package:receiptcamp/models/folder.dart';
 
 Future<void> showCreateFolderDialog(
-    BuildContext context, FolderViewCubit folderViewCubit) async {
+    BuildContext context, FolderViewCubit folderViewCubit, Folder currentFolder) async {
   return await showDialog(
       context: context,
       builder: (createFolderDialogContext) {
         return BlocProvider.value(
           value: folderViewCubit,
-          child: const FolderDialog(),
+          child: FolderDialog(currentFolder: currentFolder),
         );
       });
 }
 
 class FolderDialog extends StatefulWidget {
-  const FolderDialog({super.key});
+  final Folder currentFolder;
+
+  const FolderDialog({super.key, required this.currentFolder});
 
   @override
   State<FolderDialog> createState() => _FolderDialogState();
@@ -69,7 +72,12 @@ class _FolderDialogState extends State<FolderDialog> {
         TextButton(
           onPressed: isEnabled
               ? () {
-                  context.read<FolderViewCubit>().uploadFolder(textEditingController.value.text, 'a1');
+                print('saving folder in ${widget.currentFolder.name}');
+
+                  // 'a1' is the root folder id
+                  // this used to be:
+                  // context.read<FolderViewCubit>().uploadFolder(textEditingController.value.text,'a1')
+                  context.read<FolderViewCubit>().uploadFolder(textEditingController.value.text, widget.currentFolder.id);
                   // closing folder dialog
                   Navigator.of(context).pop();
                 }
