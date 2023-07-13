212ac9beb5831cafe995ba17bd5beb8278f57a02
Nile Kelly <nilekell@gmail.com>
2023-07-13 16:40:39 +0100

    removing folderview cubit methods/states from file system cubit

--- lib/logic/cubits/file_system/file_system_cubit.dart
+++ lib/logic/cubits/file_system/file_system_cubit.dart
@@ -1,42 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/services.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:receiptcamp/data/repositories/database_repository.dart';
-import 'package:receiptcamp/data/utils/receipt_helper.dart';
-import 'package:receiptcamp/data/utils/utilities.dart';
 import 'package:receiptcamp/models/folder.dart';
-import 'package:receiptcamp/models/receipt.dart';
-import 'package:receiptcamp/models/tag.dart';
 
 part 'file_system_state.dart';
 
-// responsible for all CRUD operations on files and folders (upload, delete, rename, move).
+// responsible for displaying folder name, back button, and upload button with correct folder reference
 class FileSystemCubit extends Cubit<FileSystemCubitState> {
   FileSystemCubit() : super(FileSystemCubitInitial());
 
-  // method to display root folder contents when the bottom navigation tab switches to file explorer
+  // method to display root folder information when the bottom navigation tab switches to file explorer
   initializeFileSystemCubit() async {
     emit(FileSystemCubitInitial());
-    fetchFiles('a1');
+    fetchFolderInformation('a1');
   }
 
-  // fetchFolderInformation(String folderId) async {
-  //   emit(FileSystemCubitLoading());
-  //   try {
-  //     final folder = await DatabaseRepository.instance.getFolderById(folderId);
-  //     emit(FileSystemCubitFolderInformationSuccess(folder: folder));
-  //   } catch (error) {
-  //     emit(FileSystemCubitError());
-  //   }
-  // }
-
-  fetchFiles(String folderId) async {
+  // method to fetch displayed folder information, which is required for FolderName, BackButton, FolderView, UploadButton
+  fetchFolderInformation(String folderId) async {
     emit(FileSystemCubitLoading());
     try {
       final folder = await DatabaseRepository.instance.getFolderById(folderId);
-      final List<Object> files = await DatabaseRepository.instance.getFolderContents(folderId);
-      emit(FileSystemCubitLoadedSuccess(files: files, folder: folder));
+      emit(FileSystemCubitFolderInformationSuccess(folder: folder));
     } catch (error) {
       emit(FileSystemCubitError());
     }
@@ -47,8 +31,8 @@ class FileSystemCubit extends Cubit<FileSystemCubitState> {
     emit(FileSystemCubitLoading());
     try {
       final folder = await DatabaseRepository.instance.getFolderById(folderId);
-      final List<Object> files = await DatabaseRepository.instance.getFolderContents(folderId);
-      emit(FileSystemCubitLoadedSuccess(files: files, folder: folder));
+      emit(FileSystemCubitFolderInformationSuccess(folder: folder));
+      // emit(FileSystemCubitLoadedSuccess(files: files, folder: folder));
     } catch (error) {
       emit(FileSystemCubitError());
     }
@@ -59,189 +43,10 @@ class FileSystemCubit extends Cubit<FileSystemCubitState> {
     emit(FileSystemCubitLoading());
     try {
       final Folder parentFolder = await DatabaseRepository.instance.getFolderById(parentFolderId);
-      final List<Object> files = await DatabaseRepository.instance.getFolderContents(parentFolderId);
-      emit(FileSystemCubitLoadedSuccess(files: files, folder: parentFolder));
+      emit(FileSystemCubitFolderInformationSuccess(folder: parentFolder));
+       // emit(FileSystemCubitLoadedSuccess(files: files, folder: folder));
     } catch (error) {
       emit(FileSystemCubitError());
-    }
-  }
-
-  // return new list of folder items whenever a folder item is deleted/moved/created
-  // consider emitting another state for above actions e.g. FileSystemCubitFolderItemsChangedState
-
-  uploadReceipt(String currentFolderId) async {
-    try {
-      final ImagePicker imagePicker = ImagePicker();
-      final XFile? receiptImage =
-          await imagePicker.pickImage(source: ImageSource.gallery);
-      if (receiptImage == null) {
-        return;
-      }
-
-      final List<dynamic> results = await ReceiptService.processingReceiptAndTags(receiptImage);
-      final Receipt receipt = results[0];
-      final List<Tag> tags = results[1];
-
-      await DatabaseRepository.instance.insertTags(tags);
-      await DatabaseRepository.instance.insertReceipt(receipt);
-      print('Image ${receipt.name} saved at ${receipt.localPath}');
-
-      final folder = await DatabaseRepository.instance.getFolderById(currentFolderId);
-      final currentFolderContents = await DatabaseRepository.instance.getFolderContents(receipt.parentId);
-      emit(FileSystemCubitUploadSuccess(uploadedName: receipt.name, folderId: folder.parentId));
-      emit(FileSystemCubitLoadedSuccess(files: currentFolderContents, folder: folder));
-    } on PlatformException catch (e) {
-      print(e.toString());
-      emit(const FileSystemCubitUploadFailure(folderId: ''));
-    } on Exception catch (e) {
-      print('Error in uploadReceipt: $e');
-      emit(const FileSystemCubitUploadFailure(folderId: ''));
-      return;
-    }
-  }
-
-  uploadReceiptFromCamera(String currentFolderId) async {
-    try {
-      final ImagePicker imagePicker = ImagePicker();
-      final XFile? receiptPicture =
-          await imagePicker.pickImage(source: ImageSource.camera);
-      if (receiptPicture == null) {
-        return;
-      }
-
-      final List<dynamic> results = await ReceiptService.processingReceiptAndTags(receiptPicture);
-      final Receipt receipt = results[0];
-      final List<Tag> tags = results[1];
-
-      await DatabaseRepository.instance.insertTags(tags);
-      await DatabaseRepository.instance.insertReceipt(receipt);
-      print('Image ${receipt.name} saved at ${receipt.localPath}');
-
-      final folder = await DatabaseRepository.instance.getFolderById(receipt.parentId);
-      final currentFolderContents = await DatabaseRepository.instance.getFolderContents(receipt.parentId);
-      emit(FileSystemCubitLoadedSuccess(files: currentFolderContents, folder: folder));
-
-      emit(FileSystemCubitUploadSuccess(uploadedName: receipt.name, folderId: receipt.parentId));
-    } on PlatformException catch (e) {
-      print(e.toString());
-      emit(const FileSystemCubitUploadFailure(folderId: ''));
-    } on Exception catch (e) {
-      print('Error in uploadReceiptFromCamera: $e');
-      emit(const FileSystemCubitUploadFailure(folderId: ''));
-      return;
-    }
-  }
-
-  uploadFolder(String folderName, String parentFolderId) async {
-    try {
-      // creating folder id
-      final folderId = Utility.generateUid();
-      final currentTime = Utility.getCurrentTime();
-
-      // create folder object
-      final folder = Folder(id: folderId, name: folderName, lastModified: currentTime, parentId: parentFolderId );
-
-      // save folder
-      DatabaseRepository.instance.insertFolder(folder);
-      print('Folder ${folder.name} saved in ${folder.parentId}');
-
-      final currentFolderContents = await DatabaseRepository.instance.getFolderContents(folder.parentId);
-
-      emit(FileSystemCubitUploadSuccess(uploadedName: folder.name, folderId: parentFolderId));
-      emit(FileSystemCubitLoadedSuccess(files: currentFolderContents, folder: folder));
-    } on Exception catch (e) {
-      print('Error in uploadFolder: $e');
-      emit(const FileSystemCubitUploadFailure(folderId: ''));
-    }
-  }
-
-  // Remainder of your new methods as requested.
-  deleteReceipt(String receiptId) async {
-    final receipt = await DatabaseRepository.instance.getReceiptById(receiptId);
-    final folder = await DatabaseRepository.instance.getFolderById(receipt.parentId);
-    try {
-      await DatabaseRepository.instance.deleteReceipt(receiptId);
-      final files = await DatabaseRepository.instance.getFolderContents(receipt.parentId);
-      emit(FileSystemCubitDeleteSuccess(deletedName: receipt.name, folderId: receipt.parentId));
-      emit(FileSystemCubitLoadedSuccess(files: files, folder: folder));
-    } on Exception catch (e) {
-      print(e.toString());
-      emit(FileSystemCubitDeleteFailure(deletedName: receipt.name, folderId: ''));
-    }
-  }
-
-  renameReceipt(Receipt receipt, String newName) async {
-    final oldName = receipt.name;
-    try {
-      await DatabaseRepository.instance.renameReceipt(receipt.id, newName);
-      emit(FileSystemCubitRenameSuccess(oldName: oldName, newName: newName, folderId: receipt.parentId));
-    } on Exception catch (e) {
-      print(e.toString());
-      emit(FileSystemCubitRenameFailure(oldName: oldName, newName: newName, folderId: ''));
-    }
-  }
-
-  moveReceipt(Receipt receipt, String targetFolderId) async {
-    try {
-      await DatabaseRepository.instance.moveReceipt(receipt, targetFolderId);
-      final folder = await DatabaseRepository.instance.getFolderById(receipt.parentId);
-      final currentFolderContents = await DatabaseRepository.instance.getFolderContents(receipt.parentId);
-      emit(FileSystemCubitMoveSuccess(oldName: receipt.name, newName: targetFolderId, folderId: folder.parentId));
-      emit(FileSystemCubitLoadedSuccess(files: currentFolderContents, folder: folder));
-    } on Exception catch (e) {
-      print(e.toString());
-      emit(FileSystemCubitMoveFailure(oldName: receipt.name, newName: targetFolderId, folderId: ''));
-    }
-  }
-
-  deleteFolder(String folderId) async {
-    final String deletedFolderName = (await DatabaseRepository.instance.getFolderById(folderId)).name;
-    final Folder deletedFolderParent = await DatabaseRepository.instance.getFolderById(folderId);
-  
-    try {
-      await DatabaseRepository.instance.deleteFolder(folderId);
-      final deletedFolderParentFiles = await DatabaseRepository.instance.getFolderContents(deletedFolderParent.id);
-      emit(FileSystemCubitDeleteSuccess(deletedName: deletedFolderName, folderId: deletedFolderParent.id));
-      emit(FileSystemCubitLoadedSuccess(files: deletedFolderParentFiles, folder: deletedFolderParent));
-    } on Exception catch (e) {
-      print(e.toString());
-      emit(FileSystemCubitDeleteFailure(deletedName: deletedFolderName, folderId: ''));
-    }
-  }
-
-  renameFolder(Folder folder, String newName) async {
-    final oldName = folder.name;
-    try {
-      await DatabaseRepository.instance.renameFolder(folder.id, newName);
-      emit(FileSystemCubitRenameSuccess(oldName: oldName, newName: newName, folderId: folder.parentId));
-    } on Exception catch (e) {
-      print(e.toString());
-      emit(FileSystemCubitRenameFailure(oldName: oldName, newName: newName, folderId: folder.parentId));
-    }
-  }
-
-  moveFolder(Folder folder, String targetFolderId) async {
-    final String targetFolderName = (await DatabaseRepository.instance.getFolderById(folder.id)).name;
-    final currentFolderContents = await DatabaseRepository.instance.getFolderContents(folder.parentId);
-
-    try {
-      await DatabaseRepository.instance.moveFolder(folder, targetFolderId);
-      emit(FileSystemCubitMoveSuccess(oldName: folder.name, newName: targetFolderName, folderId: folder.parentId));
-      emit(FileSystemCubitLoadedSuccess(files: currentFolderContents, folder: folder));
-    } on Exception catch (e) {
-      print(e.toString());
-      emit(FileSystemCubitMoveFailure(oldName: folder.name, newName: targetFolderName, folderId: folder.parentId));
-    }
-  }
-
-  refreshFiles(String folderId) async {
-    try {
-      final Folder thisFolder = await DatabaseRepository.instance.getFolderById(folderId);
-      final files = await DatabaseRepository.instance.getFolderContents(folderId);
-      emit(FileSystemCubitLoadedSuccess(files: files, folder: thisFolder));
-    } on Exception catch (e) {
-      print(e.toString());
-      emit(const FileSystemCubitRefreshFailureState(folderId: ''));
     }
   }
 }
