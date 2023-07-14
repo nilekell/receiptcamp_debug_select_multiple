ed34b42b1646655a9e484e37033535d3b6eb5daa
Nile Kelly <nilekell@gmail.com>
2023-07-14 17:48:36 +0100

    Fixed SnackBarUtility so message is updated correctly

--- lib/presentation/ui/file_explorer/snackbars/snackbar_utility.dart
+++ lib/presentation/ui/file_explorer/snackbars/snackbar_utility.dart
@@ -2,37 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:receiptcamp/logic/cubits/folder_view/folder_view_cubit.dart';
 
 abstract class SnackBarUtility {
-  static String _message = '';
-
-  static final SnackBar _appSnackBar = SnackBar(
-    content: Text(_message),
-    duration: const Duration(milliseconds: 2000),
-  );
-
   static void showSnackBar(BuildContext context, FolderViewState state) {
-    if (state is FolderViewUploadSuccess) {
-      _message = '${state.uploadedName} added successfully';
-    } else if (state is FolderViewUploadFailure) {
-      _message = 'Failed to save file object';
-    } else if (state is FolderViewRenameSuccess) {
-      _message = '${state.oldName} renamed to ${state.newName}';
-    } else if (state is FolderViewRenameFailure) {
-      _message = 'Failed to rename ${state.oldName}';
-    } else if (state is FolderViewMoveSuccess) {
-      _message = 'Moved ${state.oldName} to ${state.newName}';
-    } else if (state is FolderViewMoveFailure) {
-      _message = 'Failed to move ${state.oldName} to ${state.newName}';
-    } else if (state is FolderViewDeleteSuccess) {
-      _message = 'Deleted ${state.deletedName}';
-    } else if (state is FolderViewDeleteFailure) {
-      _message = 'Failed to delete ${state.deletedName}';
-    } else {
-      print('showFileSystemSnackBar: state is ${state.runtimeType.toString()}');
-      return;
+    String message = '';
+
+    switch (state) {
+      case FolderViewUploadSuccess():
+        message = '${state.uploadedName} added successfully';
+        break;
+      case FolderViewUploadFailure():
+        message = 'Failed to save file object';
+        break;
+      case FolderViewRenameSuccess():
+        message = '${state.oldName} renamed to ${state.newName}';
+        break;
+      case FolderViewRenameFailure():
+        message = 'Failed to rename ${state.oldName}';
+        break;
+      case FolderViewMoveSuccess():
+        message = 'Moved ${state.oldName} to ${state.newName}';
+        break;
+      case FolderViewMoveFailure():
+        message = 'Failed to move ${state.oldName} to ${state.newName}';
+        break;
+      case FolderViewDeleteSuccess():
+        message = 'Deleted ${state.deletedName}';
+        break;
+      case FolderViewDeleteFailure():
+        message = 'Failed to delete ${state.deletedName}';
+        break;
+      default:
+        print('Unknown state in showSnackBar: ${state.runtimeType}');
+        return;
     }
 
-    print(_message);
-    ScaffoldMessenger.of(context).showSnackBar(_appSnackBar);
+    final SnackBar appSnackBar = SnackBar(
+      content: Text(message),
+      duration: const Duration(milliseconds: 2000),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(appSnackBar);
   }
 }
+
 
