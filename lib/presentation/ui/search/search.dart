3b8b00215e2676b08ed206f31655076396c79fae
Nile Kelly <nilekell@gmail.com>
2023-07-24 16:32:45 +0100

    adding WIP CustomSearchDelegate with initial BlocBuilders()

---
+++ lib/presentation/ui/search/search.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:receiptcamp/logic/blocs/search/search_bloc.dart';
+
+class CustomSearchDelegate extends SearchDelegate {
+  final SearchBloc searchBloc;
+
+  CustomSearchDelegate({required this.searchBloc}) : super();
+  
+  // first overridden method to build ui that appears after search field
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          // query is the current string shown in the AppBar
+          // resetting query to empty string
+          query = '';
+        },
+        icon: const Icon(Icons.clear),
+      ),
+    ];
+  }
+
+  // second overridden method to build ui that appears before search field
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        // closing search delegate
+        close(context, null);
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  // third overridden method to show the querying process at the runtime
+  // method is called when the user submits a query
+  @override
+  Widget buildResults(BuildContext context) {
+    return BlocBuilder<SearchBloc, SearchState>(
+      builder: (context, state) {
+        return ListView(
+          children: const [
+            Text('result 1'),
+            Text('result 2'),
+            Text('result 3'),
+          ],
+        );
+      },
+    );
+  }
+
+  // last overridden method to show the querying process at the runtime
+  // method is called whenever the content of [query] changes
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    searchBloc.add(TextChanged(text: query));
+
+    return BlocBuilder<SearchBloc, SearchState>(
+      builder: (context, state) {
+        return ListView(
+          children: const [
+            Text('suggestion 1'),
+            Text('suggestion 2'),
+            Text('suggestion 3'),
+          ],
+        );
+      },
+    );
+  }
+}
