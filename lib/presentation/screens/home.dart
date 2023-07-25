ceca1a1b77aaaff0cd210713cce7dbf8346fda80
Nile Kelly <nilekell@gmail.com>
2023-07-25 21:37:36 +0100

    refactored home receipt tile to separate widget

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:receiptcamp/data/utils/utilities.dart';
 import 'package:receiptcamp/logic/blocs/home/home_bloc.dart';
+import 'package:receiptcamp/models/receipt.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -86,48 +87,7 @@ class _HomeState extends State<Home> {
                             height: MediaQuery.of(context).size.height / 3,
                             padding: const EdgeInsets.all(
                                 4.0), // padding between each card
-                            child: Card(
-                              elevation: 8.0,
-                              shape: RoundedRectangleBorder(
-                                // Card's rounded edges
-                                borderRadius: BorderRadius.circular(15.0),
-                              ),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.all(10.0),
-                                    child: Text(
-                                      displayName,
-                                      style: const TextStyle(
-                                          fontSize: 18.0,
-                                          fontWeight: FontWeight.bold),
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                  ),
-                                  Expanded(
-                                    child: ClipRRect(
-                                      // round the image corners
-                                      borderRadius: const BorderRadius.vertical(
-                                          top: Radius.circular(15.0)),
-                                      child: Image.file(
-                                        File(receipt.localPath),
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  ),
-                                  Padding(
-                                    padding: const EdgeInsets.all(10.0),
-                                    child: Text(
-                                      'Created on $displayDate',
-                                      style: const TextStyle(
-                                          fontSize: 14.0, color: Colors.grey),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
+                            child: HomeReceiptTile(displayName: displayName, receipt: receipt, displayDate: displayDate),
                           ),
                         );
                       },
@@ -140,5 +100,64 @@ class _HomeState extends State<Home> {
           return Container();
       }
     });
+  }
+}
+
+class HomeReceiptTile extends StatelessWidget {
+  const HomeReceiptTile({
+    super.key,
+    required this.displayName,
+    required this.receipt,
+    required this.displayDate,
+  });
+
+  final String displayName;
+  final Receipt receipt;
+  final String displayDate;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 8.0,
+      shape: RoundedRectangleBorder(
+        // Card's rounded edges
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              displayName,
+              style: const TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+          Expanded(
+            child: ClipRRect(
+              // round the image corners
+              borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(15.0)),
+              child: Image.file(
+                File(receipt.localPath),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              'Created on $displayDate',
+              style: const TextStyle(
+                  fontSize: 14.0, color: Colors.grey),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
