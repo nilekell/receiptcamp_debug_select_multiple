88e43209fe5f7c2994c56c3ce9cdfd8c992fe46f
Nile Kelly <nilekell@gmail.com>
2023-07-25 21:33:05 +0100

    Added initial home screen design

--- lib/presentation/screens/home.dart
+++ lib/presentation/screens/home.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:easy_image_viewer/easy_image_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:receiptcamp/data/utils/utilities.dart';
 import 'package:receiptcamp/logic/blocs/home/home_bloc.dart';
 
 class Home extends StatefulWidget {
@@ -30,26 +31,110 @@ class _HomeState extends State<Home> {
         case HomeErrorState():
           return const Text('Error showing receipts');
         case HomeEmptyReceiptsState():
-          return RefreshIndicator(
-            onRefresh: () async {
-              context.read<HomeBloc>().add(HomeInitialEvent());
-            },
-            child: const Center(child: Text('No receipts to show')),
+          return const Center(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                      height: 20), // provide some space between image and text
+                  Text(
+                    "You haven't saved any receipts yet :(",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    "To start saving receipts, navigate to folders and press the upload button",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
           );
         case HomeLoadedSuccessState():
-          return RefreshIndicator(
-              onRefresh: () async {
-                context.read<HomeBloc>().add(HomeInitialEvent());
-              },
-              child: ListView.builder(
-                  itemCount: state.receipts.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(title: Text(state.receipts[index].name.split('.').first),
-                    onTap: () {
-                      final imageProvider = Image.file(File(state.receipts[index].localPath)).image;
-                      showImageViewer(context, imageProvider);
-                    },);
-                  }));
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: RefreshIndicator(
+                    onRefresh: () async {
+                      context.read<HomeBloc>().add(HomeInitialEvent());
+                    },
+                    child: ListView.builder(
+                      itemCount: state.receipts.length,
+                      itemBuilder: (context, index) {
+                        final receipt = state.receipts[index];
+                        final formattedDateTime =
+                            Utility.formatDateTimeFromUnixTimestamp(
+                                receipt.dateCreated);
+                        final displayDate =
+                            Utility.formatDisplayDateFromDateTime(
+                                formattedDateTime);
+                        String displayName = receipt.name.length > 15
+                            ? "${receipt.name.substring(0, 15)}..."
+                            : receipt.name;
+                        return GestureDetector(
+                          onTap: () {
+                            final imageProvider =
+                                Image.file(File(receipt.localPath)).image;
+                            showImageViewer(context, imageProvider);
+                          },
+                          child: Container(
+                            color: Colors.white,
+                            height: MediaQuery.of(context).size.height / 3,
+                            padding: const EdgeInsets.all(
+                                4.0), // padding between each card
+                            child: Card(
+                              elevation: 8.0,
+                              shape: RoundedRectangleBorder(
+                                // Card's rounded edges
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Text(
+                                      displayName,
+                                      style: const TextStyle(
+                                          fontSize: 18.0,
+                                          fontWeight: FontWeight.bold),
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: ClipRRect(
+                                      // round the image corners
+                                      borderRadius: const BorderRadius.vertical(
+                                          top: Radius.circular(15.0)),
+                                      child: Image.file(
+                                        File(receipt.localPath),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Text(
+                                      'Created on $displayDate',
+                                      style: const TextStyle(
+                                          fontSize: 14.0, color: Colors.grey),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    )),
+              ),
+            ],
+          );
         default:
           print('Home Screen: ${state.toString()}');
           return Container();
