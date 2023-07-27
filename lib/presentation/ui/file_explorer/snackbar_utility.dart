1b2e6ada1510f3ba5eae64e4afd16513e122c613
Nile Kelly <nilekell@gmail.com>
2023-07-27 20:27:27 +0100

    SnackbarUtility: added quote marks around object names in message strings

--- lib/presentation/ui/file_explorer/snackbar_utility.dart
+++ lib/presentation/ui/file_explorer/snackbar_utility.dart
@@ -1,35 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:receiptcamp/logic/cubits/folder_view/folder_view_cubit.dart';
+import "package:flutter/material.dart";
+import "package:receiptcamp/logic/cubits/folder_view/folder_view_cubit.dart";
 
 // utility class that creates and shows snackbars displayed on file explorer screen
 abstract class SnackBarUtility {
   static void showSnackBar(BuildContext context, FolderViewState state) {
-    String message = '';
+    String message = "";
 
     switch (state) {
       case FolderViewUploadSuccess():
-        message = '${state.uploadedName} added successfully';
+        message = "'${state.uploadedName}' added successfully";
         break;
       case FolderViewUploadFailure():
-        message = 'Failed to save file object';
+        message = "Failed to save file object";
         break;
       case FolderViewRenameSuccess():
-        message = '${state.oldName} renamed to ${state.newName}';
+        message = "'${state.oldName}' renamed to '${state.newName}'";
         break;
       case FolderViewRenameFailure():
-        message = 'Failed to rename ${state.oldName}';
+        message = "Failed to rename '${state.oldName}'";
         break;
       case FolderViewMoveSuccess():
-        message = 'Moved ${state.oldName} to ${state.newName}';
+        message = "Moved '${state.oldName}' to '${state.newName}'";
         break;
       case FolderViewMoveFailure():
-        message = 'Failed to move ${state.oldName} to ${state.newName}';
+        message = "Failed to move '${state.oldName}' to '${state.newName}'";
         break;
       case FolderViewDeleteSuccess():
-        message = 'Deleted ${state.deletedName}';
+        message = "Deleted '${state.deletedName}'";
         break;
       case FolderViewDeleteFailure():
-        message = 'Failed to delete ${state.deletedName}';
+        message = "Failed to delete '${state.deletedName}'";
         break;
       default:
         print('Unknown state in showSnackBar: ${state.runtimeType}');
