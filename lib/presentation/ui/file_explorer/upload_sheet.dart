85044e778236f987e31f2f605757eaa3680eba27
Nile Kelly <nilekell@gmail.com>
2023-07-30 15:48:31 +0100

    UploadSheet: wrapped Containers for Icon in InkWell + Material widgets to show splash on icon tap

--- lib/presentation/ui/file_explorer/upload_sheet.dart
+++ lib/presentation/ui/file_explorer/upload_sheet.dart
@@ -36,65 +36,79 @@ class UploadOptionsBottomSheet extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.all(30.0),
-            child: Container(
-                padding: const EdgeInsets.all(5),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: Colors.white, width: 3),
-                ),
-                child: IconButton(
-                  color: iconColour,
-                    iconSize: iconSize,
-                    splashRadius: 120,
-                    onPressed: () {
-                      context
-                          .read<FolderViewCubit>()
-                          .uploadReceipt(currentFolder.id);
-                      // closing bottom sheet
-                      Navigator.of(context).pop();
-                    },
-                    icon: const Icon(Icons.photo_library)),
+            child: Material(
+              color: Colors.blue,
+              child: InkWell(
+                child: Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 3),
+                    ),
+                    child: IconButton(
+                      color: iconColour,
+                        iconSize: iconSize,
+                        onPressed: () {
+                          context
+                              .read<FolderViewCubit>()
+                              .uploadReceipt(currentFolder.id);
+                          // closing bottom sheet
+                          Navigator.of(context).pop();
+                        },
+                        icon: const Icon(Icons.photo_library)),
+                  ),
               ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Container(
-              padding: const EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.white, width: 3),
-              ),
-              child: IconButton(
-                  iconSize: iconSize,
-                  color: iconColour,
-                  onPressed: () {
-                    context
-                        .read<FolderViewCubit>()
-                        .uploadReceiptFromCamera(currentFolder.id);
-                    // closing bottom sheet
-                    Navigator.of(context).pop();
-                  },
-                  icon: const Icon(Icons.photo_camera)),
             ),
           ),
           Padding(
             padding: const EdgeInsets.all(30.0),
-            child: Container(
-              padding: const EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.white, width: 3),
+            child: Material(
+              color: Colors.blue,
+              child: InkWell(
+                child: Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 3),
+                  ),
+                  child: IconButton(
+                      iconSize: iconSize,
+                      color: iconColour,
+                      onPressed: () {
+                        context
+                            .read<FolderViewCubit>()
+                            .uploadReceiptFromCamera(currentFolder.id);
+                        // closing bottom sheet
+                        Navigator.of(context).pop();
+                      },
+                      icon: const Icon(Icons.photo_camera)),
+                ),
               ),
-              child: IconButton(
-                  iconSize: iconSize,
-                  color: iconColour,
-                  onPressed: () {
-                    // closing bottom sheet
-                    Navigator.of(context).pop();
-                    showCreateFolderDialog(context,
-                        context.read<FolderViewCubit>(), currentFolder);
-                  },
-                  icon: const Icon(Icons.create_new_folder)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Material(
+              color: Colors.blue,
+              child: InkWell(
+                child: Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 3),
+                  ),
+                  child: IconButton(
+                      iconSize: iconSize,
+                      color: iconColour,
+                      onPressed: () {
+                        // closing bottom sheet
+                        Navigator.of(context).pop();
+                        showCreateFolderDialog(context,
+                            context.read<FolderViewCubit>(), currentFolder);
+                      },
+                      icon: const Icon(Icons.create_new_folder)),
+                ),
+              ),
             ),
           ),
           const SizedBox(
