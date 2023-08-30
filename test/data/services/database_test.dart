b6f5da0ceb6d0350d3f6870a21016327db0a6d0f
Nile Kelly <nilekell@gmail.com>
2023-08-30 11:50:45 +0100

    WIP database unit tests - receipt method tests & added setUp & tearDown methods

--- test/data/services/database_test.dart
+++ test/data/services/database_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:receiptcamp/data/data_constants.dart';
 import 'package:receiptcamp/data/utils/utilities.dart';
 import 'package:receiptcamp/models/folder.dart';
+import 'package:receiptcamp/models/receipt.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:receiptcamp/data/services/database.dart';
@@ -11,6 +12,16 @@ void main() {
   databaseFactory = databaseFactoryFfi;
 
   final dbService = DatabaseService.instance;
+
+  setUp(() async {
+    // Delete all folders, receipts, and tags before each test
+    await dbService.deleteAll();
+  });
+
+  tearDown(() async {
+    // Delete all folders, receipts, and tags after each test
+    await dbService.deleteAll();
+  });
 
   group('DatabaseService', () {
     group('Initialisation', () {
@@ -235,6 +246,226 @@ void main() {
       });
     });
 
-    group('ReceiptMethods', () {});
+    group('ReceiptMethods', () {
+  test('insertReceipt', () async {
+    final receiptId = Utility.generateUid();
+    final receipt = Receipt(
+        id: receiptId,
+        name: 'testReceipt',
+        localPath: 'testPath',
+        dateCreated: Utility.getCurrentTime(),
+        lastModified: Utility.getCurrentTime(),
+        storageSize: 100,
+        parentId: rootFolderId);
+    await dbService.insertReceipt(receipt);
+    final result = await dbService.getReceiptById(receiptId);
+    expect(result.id, receiptId);
+    expect(result.name, 'testReceipt');
+  });
+
+  test('getReceiptById', () async {
+    final receiptId = Utility.generateUid();
+    final receipt = Receipt(
+        id: receiptId,
+        name: 'testReceiptById',
+        localPath: 'testPath',
+        dateCreated: Utility.getCurrentTime(),
+        lastModified: Utility.getCurrentTime(),
+        storageSize: 100,
+        parentId: rootFolderId);
+    await dbService.insertReceipt(receipt);
+    final result = await dbService.getReceiptById(receiptId);
+    expect(result.id, receiptId);
+    expect(result.name, 'testReceiptById');
+  });
+
+  test('getReceipts', () async {
+
+    // create some receipts
+    final receipt1Id = Utility.generateUid();
+    final receipt2Id = Utility.generateUid();
+
+    await dbService.insertReceipt(Receipt(
+        id: receipt1Id,
+        name: 'receipt1',
+        localPath: 'testPath1',
+        dateCreated: Utility.getCurrentTime(),
+        lastModified: Utility.getCurrentTime(),
+        storageSize: 100,
+        parentId: rootFolderId));
+    await dbService.insertReceipt(Receipt(
+        id: receipt2Id,
+        name: 'receipt2',
+        localPath: 'testPath2',
+        dateCreated: Utility.getCurrentTime(),
+        lastModified: Utility.getCurrentTime(),
+        storageSize: 100,
+        parentId: rootFolderId));
+
+    // get the list of receipts
+    final result = await dbService.getReceipts();
+
+    // check the returned list of receipt ids against the previously created list of receipt ids
+    final returnedIds = result.map((receipt) => receipt.id);
+    expect(returnedIds, containsAll([receipt1Id, receipt2Id]));
+    expect(returnedIds.length, 2);
+});
+
+  test('updateReceipt', () async {
+    final receiptId = Utility.generateUid();
+    final receiptDateCreated = Utility.getCurrentTime();
+    final receipt = Receipt(
+        id: receiptId,
+        name: 'testReceipt',
+        localPath: 'testPath',
+        dateCreated: receiptDateCreated,
+        lastModified: receiptDateCreated,
+        storageSize: 100,
+        parentId: rootFolderId);
+    await dbService.insertReceipt(receipt);
+    final changedReceiptLastModified = Utility.getCurrentTime();
+    final changedReceipt = Receipt(
+        id: receiptId,
+        name: 'updatedReceipt',
+        localPath: 'testPath',
+        dateCreated: receiptDateCreated,
+        lastModified: changedReceiptLastModified,
+        storageSize: 100,
+        parentId: rootFolderId);
+    await dbService.updateReceipt(changedReceipt);
+    final updatedReceipt = await dbService.getReceiptById(receiptId);
+
+    expect(updatedReceipt.id, receiptId);
+    expect(updatedReceipt.name, 'updatedReceipt');
+    expect(updatedReceipt.dateCreated, receiptDateCreated);
+    expect(updatedReceipt.localPath, 'testPath');
+    expect(updatedReceipt.lastModified, changedReceiptLastModified);
+    expect(updatedReceipt.storageSize, 100);
+    expect(updatedReceipt.parentId, rootFolderId);
+
+  });
+
+  test('deleteReceipt', () async {
+    final receiptId = Utility.generateUid();
+    final receipt = Receipt(
+        id: receiptId,
+        name: 'testReceipt',
+        localPath: 'testPath',
+        dateCreated: Utility.getCurrentTime(),
+        lastModified: Utility.getCurrentTime(),
+        storageSize: 100,
+        parentId: rootFolderId);
+    await dbService.insertReceipt(receipt);
+    await dbService.deleteReceipt(receiptId);
+    try {
+      await dbService.getReceiptById(receiptId);
+    } catch (e) {
+      expect(e, isA<Exception>());
+    }
+  });
+
+  test('moveReceipt', () async {
+    final targetFolderId = Utility.generateUid();
+    final targetFolder = Folder(id: targetFolderId, name: 'targetFolder', lastModified: Utility.getCurrentTime(), parentId: rootFolderId);
+    await dbService.insertFolder(targetFolder);
+    final receiptId = Utility.generateUid();
+    final receipt = Receipt(
+        id: receiptId,
+        name: 'testMoveReceipt',
+        localPath: 'testPath',
+        dateCreated: Utility.getCurrentTime(),
+        lastModified: Utility.getCurrentTime(),
+        storageSize: 100,
+        parentId: rootFolderId);
+    await dbService.insertReceipt(receipt);
+    await dbService.moveReceipt(receipt, targetFolderId);
+    final movedReceipt = await dbService.getReceiptById(receiptId);
+    expect(movedReceipt.parentId, targetFolderId);
+  });
+
+    test('renameReceipt', () async {
+      // Create a new receipt
+      final receiptId = Utility.generateUid();
+      final receipt = Receipt(
+        id: receiptId,
+        name: 'testRenameReceipt',
+        localPath: 'testPath',
+        dateCreated: Utility.getCurrentTime(),
+        lastModified: Utility.getCurrentTime(),
+        storageSize: 100,
+        parentId: rootFolderId);
+
+      // Insert the receipt into the database
+      await dbService.insertReceipt(receipt);
+
+      // Rename the receipt
+      await dbService.renameReceipt(receipt.id, 'newName');
+
+      // Retrieve the receipt from the database
+      final renamedReceipt = await dbService.getReceiptById(receipt.id);
+
+      // Check if the receipt's name has been updated
+      expect(renamedReceipt.name, 'newName');
+    });
+
+    test('getRecentReceipts', () async {
+      // Create 10 receipts
+      for (int i = 0; i < 10; i++) {
+        final receipt = Receipt(
+          id: 'testId$i',
+          name: 'testName$i',
+          localPath: 'testPath$i',
+          dateCreated: i < 8 ? Utility.getCurrentTime() : 1000, // set old timestamp for last two receipts
+          lastModified: i < 8 ? Utility.getCurrentTime() : 1000, // set old timestamp for last two receipts
+          storageSize: 100,
+          parentId: 'testParentId',
+        );
+
+        // Insert the receipt into the database
+        await dbService.insertReceipt(receipt);
+      }
+
+      // Retrieve the 8 most recently created receipts
+      final recentReceipts = await dbService.getRecentReceipts();
+
+      // Check if the correct number of receipts has been retrieved
+      expect(recentReceipts.length, 8);
+
+      // Check if the retrieved receipts are in the correct order
+      for (int i = 0; i < 8; i++) {
+        expect(recentReceipts[i].name, 'testName$i');
+      }
+
+      // Check if the retrieved receipts do not contain the two receipts with the old timestamps
+      for (var receipt in recentReceipts) {
+        expect(receipt.dateCreated, isNot(1000));
+        expect(receipt.lastModified, isNot(1000));
+      }
+    });
+
+    test('getReceiptByName', () async {
+
+      final receiptId = Utility.generateUid();
+      final receipt = Receipt(
+        id: receiptId,
+        name: 'testGetByNameReceipt',
+        localPath: 'testPath',
+        dateCreated: Utility.getCurrentTime(),
+        lastModified: Utility.getCurrentTime(),
+        storageSize: 100,
+        parentId: rootFolderId);
+
+      // Insert the receipt into the database
+      await dbService.insertReceipt(receipt);
+
+      // Retrieve the receipt by its name
+      final retrievedReceipts = await dbService.getReceiptByName(receipt.name);
+
+      // Check if the correct receipt has been retrieved
+      expect(retrievedReceipts[0].id, receipt.id);
+    });
+
+});
   });
 }
+
