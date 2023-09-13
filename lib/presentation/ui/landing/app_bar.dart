06a3b7861d79e3e9c75a0c790c7f06b437271d84
Nile Kelly <nilekell@gmail.com>
2023-09-13 17:11:09 +0100

    AppBar: redesign

--- lib/presentation/ui/landing/app_bar.dart
+++ lib/presentation/ui/landing/app_bar.dart
@@ -4,7 +4,7 @@ import 'package:receiptcamp/logic/blocs/search/search_bloc.dart';
 import 'package:receiptcamp/presentation/ui/search/search.dart';
 import 'package:receiptcamp/presentation/ui/ui_constants.dart';
 
-class HomeAppBar extends StatelessWidget implements PreferredSizeWidget{
+class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   const HomeAppBar({super.key});
 
   @override
@@ -13,15 +13,47 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget{
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      backgroundColor: Colors.blue,
-      title: const Text(appName,
-      style: TextStyle(fontSize: 27),),
+      flexibleSpace: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(primaryDarkBlue),
+              Color(primaryLightBlue),
+            ],
+            stops: [0.0, 0.75],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+      ),
+      title: const Row(
+        mainAxisSize: MainAxisSize.min, // centers title by reducing width of row
+        children: <Widget>[
+          Text(
+            'Receipt',
+            style: TextStyle(
+              fontWeight: FontWeight.bold, // Medium weight
+              color: Colors.white,
+              fontSize: 27,
+            ),
+          ),
+          Text(
+            'Camp',
+            style: TextStyle(
+              fontWeight: FontWeight.normal, // Regular weight
+              color: Colors.white,
+              fontSize: 27,
+            ),
+          ),
+        ],
+      ),
       actions: [
         IconButton(
           onPressed: () {
             showSearch(
-              context: context, 
-              delegate: CustomSearchDelegate(searchBloc: context.read<SearchBloc>()));
+                context: context,
+                delegate: CustomSearchDelegate(
+                    searchBloc: context.read<SearchBloc>()));
           },
           icon: const Icon(Icons.search),
         )
