84d3ba41a3f037603c0ebb5d042e96214b0d12b1
Nile Kelly <nilekell@gmail.com>
2023-09-13 14:40:35 +0100

    DeleteReceiptDialog: redesign

--- lib/presentation/ui/file_explorer/receipt/delete_receipt_confirmation_dialog.dart
+++ lib/presentation/ui/file_explorer/receipt/delete_receipt_confirmation_dialog.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:receiptcamp/logic/cubits/folder_view/folder_view_cubit.dart';
 import 'package:receiptcamp/models/receipt.dart';
+import 'package:receiptcamp/presentation/ui/ui_constants.dart';
 
 Future<void> showDeleteReceiptDialog(BuildContext context,
     FolderViewCubit folderViewCubit, Receipt receipt) async {
@@ -9,9 +10,7 @@ Future<void> showDeleteReceiptDialog(BuildContext context,
     context: context,
     builder: (deleteReceiptDialogContext) {
       return BlocProvider.value(
-        value: folderViewCubit,
-        child: DeleteReceiptDialog(receipt: receipt)
-      );
+          value: folderViewCubit, child: DeleteReceiptDialog(receipt: receipt));
     },
   );
 }
@@ -19,24 +18,37 @@ Future<void> showDeleteReceiptDialog(BuildContext context,
 class DeleteReceiptDialog extends StatelessWidget {
   final Receipt receipt;
 
-  const DeleteReceiptDialog({
-    super.key, required this.receipt
-  });
+  DeleteReceiptDialog({super.key, required this.receipt});
+
+  final ButtonStyle textButtonStyle =
+      TextButton.styleFrom(foregroundColor: Colors.white);
+
+  final TextStyle actionButtonTextStyle = const TextStyle(fontSize: 18);
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: const Text('Delete forever?', textAlign: TextAlign.left, style: TextStyle(fontWeight: FontWeight.w500),),
-      content: Text('${receipt.name} will be deleted forever.'),
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(40.0))),
+      backgroundColor: const Color(primaryDarkBlue),
+      title: const Text(
+        'Delete forever?',
+        textAlign: TextAlign.left,
+        style: TextStyle(fontWeight: FontWeight.w700, color: Colors.white),
+      ),
+      content: Text('${receipt.name} will be deleted forever.',
+          style: const TextStyle(color: Colors.white)),
       actions: <Widget>[
         TextButton(
-          child: const Text('Cancel'),
+          style: textButtonStyle,
+          child: Text('Cancel', style: actionButtonTextStyle),
           onPressed: () {
             Navigator.of(context).pop();
           },
         ),
         TextButton(
-          child: const Text('Delete'),
+          style: textButtonStyle,
+          child: Text('Delete', style: actionButtonTextStyle),
           onPressed: () {
             context.read<FolderViewCubit>().deleteReceipt(receipt.id);
             Navigator.of(context).pop();
