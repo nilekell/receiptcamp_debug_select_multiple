8470a6cbe5f9de9f85295ab80d68014207cca3f1
Nile Kelly <nilekell@gmail.com>
2023-09-16 12:57:50 +0100

    BottomNavigationBar: ui redesign

--- lib/presentation/ui/landing/nav_bar.dart
+++ lib/presentation/ui/landing/nav_bar.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:receiptcamp/logic/cubits/landing/landing_cubit.dart';
+import 'package:receiptcamp/presentation/ui/ui_constants.dart';
 
 Widget bottomNavigationBar(int state, BuildContext context) {
-    return BottomNavigationBar(
-      backgroundColor: Colors.blue,
+  const double receiptImageScale = 1.3;
+
+  return BottomNavigationBar(
+      backgroundColor: const Color(primaryDarkBlue),
       selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.black45,
-      items: const [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+      unselectedItemColor: const Color(primaryLightBlue),
+      items: [
+        const BottomNavigationBarItem(
+            icon: Icon(Icons.home_rounded, size: 50,),
             label: ''),
+        // Image.asset() colours can't be controlled by BottomNavigationBar selected/unselected item colour so its colours are controlled
+        // by BottomNavigationBarItem icon and activeIcon
         BottomNavigationBarItem(
-            icon: Icon(Icons.receipt_long_outlined),
-            label: ''),
+            icon: Image.asset('assets/receipt.png', colorBlendMode: BlendMode.srcIn, color: const Color(primaryLightBlue), scale: receiptImageScale,),
+            label: '',
+            activeIcon: Image.asset('assets/receipt.png', colorBlendMode: BlendMode.srcIn, color: Colors.white, scale: receiptImageScale,)),
       ],
       currentIndex: state,
       onTap: (value) => context.read<LandingCubit>().updateIndex(value),
     );
-  }
+}
