a5bb2fc8371e29a7f31ad78ad1daa28dd1c64cbb
Nile Kelly <nilekell@gmail.com>
2023-09-24 13:24:10 +0100

    BottomNavigationBar: added offset to icons to increase distance from top of bottom nav bar

--- lib/presentation/ui/landing/nav_bar.dart
+++ lib/presentation/ui/landing/nav_bar.dart
@@ -4,24 +4,47 @@ import 'package:receiptcamp/logic/cubits/landing/landing_cubit.dart';
 import 'package:receiptcamp/presentation/ui/ui_constants.dart';
 
 Widget bottomNavigationBar(int state, BuildContext context) {
+  // increasing scale, reduces receipt icon size & vice-versa
   const double receiptImageScale = 1.3;
+  // moves icon down by x number of pixels without affecting the layout (and other widgets)
+  const Offset iconOffset = Offset(0, 15);
 
   return BottomNavigationBar(
-      backgroundColor: const Color(primaryDarkBlue),
-      selectedItemColor: Colors.white,
-      unselectedItemColor: const Color(primaryLightBlue),
-      items: [
-        const BottomNavigationBarItem(
-            icon: Icon(Icons.home_rounded, size: 50,),
-            label: ''),
-        // Image.asset() colours can't be controlled by BottomNavigationBar selected/unselected item colour so its colours are controlled
-        // by BottomNavigationBarItem icon and activeIcon
-        BottomNavigationBarItem(
-            icon: Image.asset('assets/receipt.png', colorBlendMode: BlendMode.srcIn, color: const Color(primaryLightBlue), scale: receiptImageScale,),
-            label: '',
-            activeIcon: Image.asset('assets/receipt.png', colorBlendMode: BlendMode.srcIn, color: Colors.white, scale: receiptImageScale,)),
-      ],
-      currentIndex: state,
-      onTap: (value) => context.read<LandingCubit>().updateIndex(value),
-    );
+    backgroundColor: const Color(primaryDarkBlue),
+    selectedItemColor: Colors.white,
+    unselectedItemColor: const Color(primaryLightBlue),
+    items: [
+      BottomNavigationBarItem(
+          icon: Transform.translate(
+            offset: iconOffset,
+            child: const Icon(
+              Icons.home_rounded,
+              size: 50,
+            ),
+          ),
+          label: ''),
+      BottomNavigationBarItem(
+          icon: Transform.translate(
+              offset: iconOffset,
+              // Image.asset() colours can't be adjusted by BottomNavigationBar selected/unselected item colour so its colours are controlled
+              // by BottomNavigationBarItem icon and activeIcon
+              child: Image.asset(
+                'assets/receipt.png',
+                colorBlendMode: BlendMode.srcIn,
+                color: const Color(primaryLightBlue),
+                scale: receiptImageScale,
+              )),
+          label: '',
+          activeIcon: Transform.translate(
+              offset: iconOffset,
+              child: Image.asset(
+                'assets/receipt.png',
+                colorBlendMode: BlendMode.srcIn,
+                color: Colors.white,
+                scale: receiptImageScale,
+              ))),
+    ],
+    currentIndex: state,
+    onTap: (value) => context.read<LandingCubit>().updateIndex(value),
+  );
 }
