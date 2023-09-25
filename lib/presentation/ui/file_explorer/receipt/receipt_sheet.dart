638de38af72206bf1a6394f5907022518d4602c5
Nile Kelly <nilekell@gmail.com>
2023-09-25 23:12:04 +0100

    FolderSheet, ReceiptSheet: converted to DraggableScrollableSheet

--- lib/presentation/ui/file_explorer/receipt/receipt_sheet.dart
+++ lib/presentation/ui/file_explorer/receipt/receipt_sheet.dart
@@ -15,147 +15,154 @@ void showReceiptOptions(
   const double iconPadding = 8.0;
 
   showModalBottomSheet(
+    isScrollControlled: true,
     backgroundColor: const Color(primaryDeepBlue),
     shape: RoundedRectangleBorder(
       borderRadius: BorderRadius.circular(20.0),
     ),
     context: context,
-    builder: (bottomSheetContext) => Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const SizedBox(
-          height: 5,
-        ),
-        ListTile(
-          leading: Transform.scale(
-            scale: 0.8,
-            child: Image.asset(
-              'assets/receipt.png',
-              color: secondaryColour,
-              colorBlendMode: BlendMode.srcIn,
-            ),
+    builder: (bottomSheetContext) => DraggableScrollableSheet(
+      initialChildSize: 0.5, // initial bottom sheet height
+      minChildSize: 0.25, // minimum possible bottom sheet height
+      maxChildSize:
+          0.8, // defines how high you can drag the the bottom sheet in relation to the screen height
+      expand: false,
+      builder: (context, scrollController) => ListView(
+        controller: scrollController,
+        children: [
+          const SizedBox(
+            height: 5,
           ),
-          title: Text(
-            receipt.name.split('.').first,
-            style: const TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w800,
-                color: secondaryColour),
-          ),
-        ),
-        const Divider(
-          thickness: 1,
-          endIndent: 20,
-          indent: 20,
-          color: secondaryColour,
-        ),
-        ListTile(
-          leading: Padding(
-            padding: const EdgeInsets.only(left: iconPadding),
-            child: Transform.scale(
-              scale: iconScale,
+          ListTile(
+            leading: Transform.scale(
+              scale: 0.8,
               child: Image.asset(
-                'assets/pencil.png',
+                'assets/receipt.png',
                 color: secondaryColour,
                 colorBlendMode: BlendMode.srcIn,
               ),
             ),
+            title: Text(
+              receipt.name.split('.').first,
+              style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800,
+                  color: secondaryColour),
+            ),
           ),
-          title: const Text(
-            'Rename',
-            style: textStyle,
+          const Divider(
+            thickness: 1,
+            endIndent: 20,
+            indent: 20,
+            color: secondaryColour,
           ),
-          onTap: () {
-            // closing bottom sheet
-            Navigator.of(bottomSheetContext).pop();
-            // opening rename receipt dialog
-            showRenameReceiptDialog(
-                bottomSheetContext, folderViewCubit, receipt);
-          },
-        ),
-        ListTile(
-          leading: Padding(
+          ListTile(
+            leading: Padding(
               padding: const EdgeInsets.only(left: iconPadding),
               child: Transform.scale(
-                scale: 0.8,
+                scale: iconScale,
                 child: Image.asset(
-                  'assets/folder_move.png',
+                  'assets/pencil.png',
                   color: secondaryColour,
                   colorBlendMode: BlendMode.srcIn,
                 ),
-              )),
-          title: const Text(
-            'Move',
-            style: textStyle,
+              ),
+            ),
+            title: const Text(
+              'Rename',
+              style: textStyle,
+            ),
+            onTap: () {
+              // closing bottom sheet
+              Navigator.of(bottomSheetContext).pop();
+              // opening rename receipt dialog
+              showRenameReceiptDialog(
+                  bottomSheetContext, folderViewCubit, receipt);
+            },
           ),
-          onTap: () {
-            Navigator.of(bottomSheetContext).pop();
-            // show move receipt dialog
-            showMoveReceiptDialog(context, folderViewCubit, receipt);
-          },
-        ),
-        ListTile(
-          leading: Padding(
-            padding: const EdgeInsets.only(left: iconPadding),
-            child: Transform.scale(
-                scale: iconScale,
-                child: Image.asset(
-                  'assets/bin.png',
-                  color: secondaryColour,
-                  colorBlendMode: BlendMode.srcIn,
+          ListTile(
+            leading: Padding(
+                padding: const EdgeInsets.only(left: iconPadding),
+                child: Transform.scale(
+                  scale: 0.8,
+                  child: Image.asset(
+                    'assets/folder_move.png',
+                    color: secondaryColour,
+                    colorBlendMode: BlendMode.srcIn,
+                  ),
                 )),
+            title: const Text(
+              'Move',
+              style: textStyle,
+            ),
+            onTap: () {
+              Navigator.of(bottomSheetContext).pop();
+              // show move receipt dialog
+              showMoveReceiptDialog(context, folderViewCubit, receipt);
+            },
           ),
-          title: const Text(
-            'Delete',
-            style: textStyle,
+          ListTile(
+            leading: Padding(
+              padding: const EdgeInsets.only(left: iconPadding),
+              child: Transform.scale(
+                  scale: iconScale,
+                  child: Image.asset(
+                    'assets/bin.png',
+                    color: secondaryColour,
+                    colorBlendMode: BlendMode.srcIn,
+                  )),
+            ),
+            title: const Text(
+              'Delete',
+              style: textStyle,
+            ),
+            onTap: () {
+              Navigator.of(bottomSheetContext).pop();
+              // opening delete receipt dialog
+              showDeleteReceiptDialog(
+                  bottomSheetContext, folderViewCubit, receipt);
+            },
           ),
-          onTap: () {
-            Navigator.of(bottomSheetContext).pop();
-            // opening delete receipt dialog
-            showDeleteReceiptDialog(
-                bottomSheetContext, folderViewCubit, receipt);
-          },
-        ),
-        ListTile(
-          leading: Padding(
-            padding: const EdgeInsets.only(left: iconPadding),
-            child: Transform.scale(
-                scale: iconScale,
-                child: Image.asset(
-                  'assets/download.png',
-                  color: secondaryColour,
-                  colorBlendMode: BlendMode.srcIn,
-                )),
+          ListTile(
+            leading: Padding(
+              padding: const EdgeInsets.only(left: iconPadding),
+              child: Transform.scale(
+                  scale: iconScale,
+                  child: Image.asset(
+                    'assets/download.png',
+                    color: secondaryColour,
+                    colorBlendMode: BlendMode.srcIn,
+                  )),
+            ),
+            title: const Text(
+              'Download',
+              style: textStyle,
+            ),
+            onTap: () {
+              Navigator.of(bottomSheetContext).pop();
+              FileService.saveImageToCameraRoll(receipt);
+            },
           ),
-          title: const Text(
-            'Download',
-            style: textStyle,
+          ListTile(
+            leading: Padding(
+              padding: const EdgeInsets.only(left: iconPadding),
+              child: Transform.scale(
+                  scale: iconScale,
+                  child: Image.asset(
+                    'assets/share.png',
+                    color: secondaryColour,
+                    colorBlendMode: BlendMode.srcIn,
+                  )),
+            ),
+            title: const Text(
+              'Share',
+              style: textStyle,
+            ),
+            onTap: () {
+              Navigator.of(bottomSheetContext).pop();
+              FileService.shareReceipt(receipt);
+            },
           ),
-          onTap: () {
-            Navigator.of(bottomSheetContext).pop();
-            FileService.saveImageToCameraRoll(receipt);
-          },
-        ),
-        ListTile(
-          leading: Padding(
-            padding: const EdgeInsets.only(left: iconPadding),
-            child: Transform.scale(
-                scale: iconScale,
-                child: Image.asset(
-                  'assets/share.png',
-                  color: secondaryColour,
-                  colorBlendMode: BlendMode.srcIn,
-                )),
-          ),
-          title: const Text(
-            'Share',
-            style: textStyle,
-          ),
-          onTap: () {
-            Navigator.of(bottomSheetContext).pop();
-            FileService.shareReceipt(receipt);
-          },
-        ),
         const SizedBox(height: 50)
       ],
     ),
