4deed13f343dae7346c772ea29d274a1256d70f7
Nile Kelly <nilekell@gmail.com>
2023-09-25 16:54:07 +0100

    Receipt: updated model to reflect new db schema + added localPath getter

--- lib/models/receipt.dart
+++ lib/models/receipt.dart
@@ -1,37 +1,37 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
+import 'package:receiptcamp/data/services/document_path_provider.dart';
 
 // class to model receipts to be stored in sql db
 class Receipt {
-    final String id;
-    final String name;
-    final String localPath;
-    final int dateCreated;
-    final int lastModified;
-    // this is in bytes
-    final int storageSize;
-    // the id of the folder that holds this receipt
-    final String parentId;
+  final String id;
+  final String name;
+  final String fileName;
+  final int dateCreated;
+  final int lastModified;
+  // this is in bytes
+  final int storageSize;
+  // the id of the folder that holds this receipt
+  final String parentId;
 
-    const Receipt({
-      required this.id,
+  // builds and returns the full path of the receipt's image based on the app's
+  // current application document directory path
+  String get localPath => '${DocumentDirectoryProvider.instance.appDocDirPath}/$fileName';
+
+  Receipt(
+      {required this.id,
       required this.name,
-      required this.localPath,
+      required this.fileName,
       required this.dateCreated,
       required this.lastModified,
       required this.storageSize,
-      required this.parentId
-    });
-
-    
-
-  
+      required this.parentId});
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'id': id,
       'name': name,
-      'localPath': localPath,
+      'fileName': fileName,
       'dateCreated': dateCreated,
       'lastModified': lastModified,
       'storageSize': storageSize,
@@ -41,14 +41,13 @@ class Receipt {
 
   factory Receipt.fromMap(Map<String, dynamic> map) {
     return Receipt(
-      id: map['id'] as String,
-      name: map['name'] as String,
-      localPath: map['localPath'] as String,
-      dateCreated: map['dateCreated'] as int,
-      lastModified: map['lastModified'] as int,
-      storageSize: map['storageSize'] as int,
-      parentId: map['parentId'] as String
-    );
+        id: map['id'] as String,
+        name: map['name'] as String,
+        fileName: map['fileName'] as String,
+        dateCreated: map['dateCreated'] as int,
+        lastModified: map['lastModified'] as int,
+        storageSize: map['storageSize'] as int,
+        parentId: map['parentId'] as String);
   }
 
   String toJson() => json.encode(toMap());
