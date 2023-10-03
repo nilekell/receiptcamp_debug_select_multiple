bd8205ab9f2b358d124fe4d7f28f5bf493256307
Nile Kelly <nilekell@gmail.com>
2023-10-03 17:13:50 +0100

    SnackBarUtility: added FolderViewPermissionsFailure case

--- lib/presentation/ui/file_explorer/snackbar_utility.dart
+++ lib/presentation/ui/file_explorer/snackbar_utility.dart
@@ -1,11 +1,12 @@
 import "package:flutter/material.dart";
+import "package:receiptcamp/data/services/permissons.dart";
 import "package:receiptcamp/data/utils/receipt_helper.dart";
 import "package:receiptcamp/logic/cubits/folder_view/folder_view_cubit.dart";
 import "package:receiptcamp/presentation/ui/ui_constants.dart";
 
 // utility class that creates and shows snackbars displayed on file explorer screen
 abstract class SnackBarUtility {
-  static void showSnackBar(BuildContext context, FolderViewState state) {
+  static void showSnackBar(BuildContext context, FolderViewActionState state) {
     String message = "";
 
     switch (state) {
@@ -48,6 +49,14 @@ abstract class SnackBarUtility {
       case FolderViewShareFailure():
         message = "Failed to share '${state.folderName}' - folder is empty ";
         break;
+      case FolderViewPermissionsFailure():
+        if (state.permissionResult == PermissionFailedResult.invalidCamera) {
+          message = 'Failed to open camera - please check app settings';
+          break;
+        } else if (state.permissionResult == PermissionFailedResult.invalidPhotos) {
+          message = 'Failed to open photo library - please check app settings';
+          break;
+        }
       default:
         print('Unknown state in showSnackBar: ${state.runtimeType}');
         return;
