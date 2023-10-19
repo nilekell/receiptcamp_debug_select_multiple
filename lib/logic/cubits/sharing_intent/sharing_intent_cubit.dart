b1f31bec9f1f6d79167fb66c153a7a06e9b10aa8
Nile Kelly <nilekell@gmail.com>
2023-10-19 18:36:30 +0100

    SharingIntentCubit: WIP

--- lib/logic/cubits/sharing_intent/sharing_intent_cubit.dart
+++ lib/logic/cubits/sharing_intent/sharing_intent_cubit.dart
@@ -4,57 +4,119 @@ import 'dart:io';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:receiptcamp/data/repositories/database_repository.dart';
+import 'package:receiptcamp/data/utils/receipt_helper.dart';
+import 'package:receiptcamp/logic/blocs/home/home_bloc.dart';
+import 'package:receiptcamp/logic/cubits/file_system/file_system_cubit.dart';
+import 'package:receiptcamp/logic/cubits/landing/landing_cubit.dart';
 import 'package:receiptcamp/models/folder.dart';
+import 'package:receiptcamp/models/receipt.dart';
+import 'package:receiptcamp/models/tag.dart';
+import 'package:share_plus/share_plus.dart';
 part 'sharing_intent_state.dart';
 
 
 class SharingIntentCubit extends Cubit<SharingIntentState> {
 
-  SharingIntentCubit({required this.mediaStream, required this.initialMedia}) : super(SharingIntentFilesRecieved());
+  SharingIntentCubit({required this.homeBloc, required this.fileSystemCubit, required this.mediaStream, required this.initialMedia, required this.landingCubit}) : super(SharingIntentFilesInitial());
+
+  final HomeBloc homeBloc;
+  final FileSystemCubit fileSystemCubit;
+  final LandingCubit landingCubit;
 
   Stream<List<File>> mediaStream;
   Future<List<File>> initialMedia;
 
-  List<File> files = <File>[];
-
   void init() async {
-    print('SharingIntentCubit instantiated');
-    mediaStream.listen((sharedFiles) {
-      for (final f in sharedFiles) {
-        files.add(f);
-      }
-
-      emit(SharingIntentFilesRecieved());
-      print("Received shared files: $sharedFiles");
-      return;
-    }, onError:(error) {
-      return;
-    },);
-
-    List<File> initialSharedFiles = await initialMedia;
-    if (initialSharedFiles.isNotEmpty) {
-      print("Initial shared files: $initialSharedFiles");
-      files = initialSharedFiles;
-      emit(SharingIntentFilesRecieved());
-      return;
-    }
-  }
-
-  void getFilesAndFolders() async {
-    emit(SharingIntentLoading());
+    emit(SharingIntentFilesInitial());
     try {
-      final receiptFiles = files;
-      final folders = await DatabaseRepository.instance.getFolders();
-      emit(SharingIntentSuccess(folders: folders, files: receiptFiles));
+      // print('SharingIntentCubit instantiated');
+
+      List<File> files = <File>[];
+
+      mediaStream.listen(
+        (sharedFiles) {
+          for (final f in sharedFiles) {
+            files.add(f);
+          }
+
+          emit(SharingIntentFilesRecieved(files: files));
+
+          if (sharedFiles.isEmpty) {
+            emit(SharingIntentNoValidFiles());
+            return;
+          }
+
+          // print("Received shared stream files: $sharedFiles");
+          getFolders();
+          return;
+        },
+        onError: (error) {
+          print(error.toString());
+          emit(SharingIntentError());
+          files.clear();
+          return;
+        },
+      );
+
+      List<File> initialSharedFiles = await initialMedia;
+      if (initialSharedFiles.isNotEmpty) {
+        // print("Received shared initial files: $initialSharedFiles");
+        files = initialSharedFiles;
+        emit(SharingIntentFilesRecieved(files: files));
+        getFolders();
+        return;
+      }
     } on Exception catch (e) {
       print(e.toString());
       emit(SharingIntentError());
     }
-
   }
 
-  void insertReceiptsIntoFolder(String folderId) {
-    // maybe use isolate method
+  void getFolders() async {
+    emit(SharingIntentLoading());
+    try {
+      final folders = await DatabaseRepository.instance.getFolders();
+      emit(SharingIntentSuccess(folders: folders));
+    } on Exception catch (e) {
+      print(e.toString());
+      emit(SharingIntentError());
+    }
+  }
+
+  void insertReceiptsIntoFolder(String folderId, List<File> receiptFiles) async {
+    emit(const SharingIntentSavingReceipts(folders: []));
+
+    List<Receipt> savedReceipts = [];
+
+    try{
+      // iterating over images and uploading them as receipts consecutively
+      // REFACTOR TO PROCESS IN BACKGROUND USING ISOLATE depending on number of files
+      for (final file in receiptFiles) {
+        final XFile receiptDocument = XFile(file.path);
+        final List<dynamic> results =
+            await ReceiptService.processingReceiptAndTags(
+                receiptDocument, folderId);
+        final Receipt receipt = results[0];
+        final List<Tag> tags = results[1];
+
+        await DatabaseRepository.instance.insertTags(tags);
+        await DatabaseRepository.instance.insertReceipt(receipt);
+
+        savedReceipts.add(receipt);
+      }
+
+      // notifying home bloc to reload after all receipts imported
+      homeBloc.add(HomeLoadReceiptsEvent());
+      // directly navigating to FileExplorer tab
+      landingCubit.updateIndex(1);
+      // navigating to parent folder in FileExplorer when RecieveReceiptView is closed
+      // notifying fileSystemCubit to reload after all receipts imported
+      fileSystemCubit.selectFolder(folderId);
+      emit(SharingIntentClose(folders: const [], savedReceipts: savedReceipts));
+    } on Exception catch (e) {
+      print(e.toString());
+      emit(SharingIntentError());
+    }
   }
 
 }
--- lib/logic/cubits/sharing_intent/sharing_intent_state.dart
+++ lib/logic/cubits/sharing_intent/sharing_intent_state.dart
@@ -7,16 +7,38 @@ sealed class SharingIntentState extends Equatable {
   List<Object> get props => [];
 }
 
-final class SharingIntentFilesRecieved extends SharingIntentState {}
+final class SharingIntentFilesInitial extends SharingIntentState {}
+
+final class SharingIntentFilesRecieved extends SharingIntentState {
+  final List<File> files;
+
+  const SharingIntentFilesRecieved({required this.files});
+
+  @override
+  List<Object> get props => [files];
+  
+}
 
 final class SharingIntentLoading extends SharingIntentState {}
 
-final class SharingIntentSuccess extends SharingIntentState {
-  final List<Folder> folders;
-  final List<File> files;
-
-  const SharingIntentSuccess({required this.folders, required this.files});
-
-}
+final class SharingIntentNoValidFiles extends SharingIntentState {}
 
 final class SharingIntentError extends SharingIntentState {}
+
+final class SharingIntentSuccess extends SharingIntentState {
+  final List<Folder> folders;
+
+  const SharingIntentSuccess({required this.folders});
+
+  @override
+  List<Object> get props => [folders];
+}
+
+final class SharingIntentSavingReceipts extends SharingIntentSuccess {
+  const SharingIntentSavingReceipts({required super.folders});
+}
+
+final class SharingIntentClose extends SharingIntentSuccess {
+  final List<Receipt> savedReceipts;
+  const SharingIntentClose({required super.folders, required this.savedReceipts});
+}
