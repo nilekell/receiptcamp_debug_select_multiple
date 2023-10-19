d204d3ff4024a7a8a31d6808e6a1ed886b2db720
Nile Kelly <nilekell@gmail.com>
2023-10-19 16:44:36 +0100

    WIP: SharingIntentService

--- lib/data/services/sharing_intent.dart
+++ lib/data/services/sharing_intent.dart
@@ -1,7 +1,9 @@
+// ignore_for_file: unused_local_variable
 import 'dart:io';
 import 'package:path/path.dart';
 import 'package:receiptcamp/data/services/directory_path_provider.dart';
 import 'package:receive_sharing_intent/receive_sharing_intent.dart';
+import 'package:receiptcamp/data/utils/receipt_helper.dart';
 
 class SharingIntentService {
   // singleton
@@ -12,22 +14,23 @@ class SharingIntentService {
 
   static SharingIntentService get instance => _instance;
 
-  final RegExp imageFileExtensionRegExp = RegExp(r'\.(jpe?g|png)$', caseSensitive: false);
+  final RegExp imageFileExtensionRegExp =
+      RegExp(r'\.(jpe?g|png)$', caseSensitive: false);
 
   // For sharing images coming from outside the app while the app is in the memory
   Stream<List<File>> get mediaStream {
-    print('get mediaStream');
     return ReceiveSharingIntent.getMediaStream().asyncMap(_mapSharedMediaFiles);
   }
 
   // For sharing images coming from outside the app while the app is closed
   Future<List<File>> get initialMedia {
-    print('get initialMedia');
     return ReceiveSharingIntent.getInitialMedia().then(_mapSharedMediaFiles);
   }
 
-  Future<List<File>> _mapSharedMediaFiles(List<SharedMediaFile> sharedMediaFiles) async {
-    print('_mapSharedMediaFiles');
+  Future<List<File>> _mapSharedMediaFiles(
+      List<SharedMediaFile> sharedMediaFiles) async {
+
+    print('sharedMediaFiles count: ${sharedMediaFiles.length}');
     List<File> sharedFiles = [];
 
     File tempImage;
@@ -36,24 +39,34 @@ class SharingIntentService {
       print('sharedMediaFile: ${file.path}');
       File sharedImage = File(file.path);
       String sharedImageExtension = extension(sharedImage.path);
-      if (imageFileExtensionRegExp.hasMatch(sharedImageExtension)) {
-        // copying images to temporary directory folder
-        String newTempPath =
-              '${DirectoryPathProvider.instance.tempDirPath}/${(basename(sharedImage.path).toLowerCase())}';
-          tempImage = await sharedImage.copy(newTempPath);
-          sharedImage.delete();
-          print('saving file: ${basename(tempImage.path)}');
-          // deleting files in share extension folder
-          sharedFiles.add(tempImage);
-      } else {
-        // skipping over images that aren't jpeg or png
+
+      // skipping over and deleting images that aren't jpeg or png
+      if (!imageFileExtensionRegExp.hasMatch(sharedImageExtension)) {
+        print('discarded ${basename(sharedImage.path)}: image is not jpeg/png');
+        sharedImage.delete();
         continue;
       }
+
+      // skipping over and deleting images that don't pass receipt validation
+      final (validImage as bool, invalidImageReason as ValidationError) =
+          await ReceiptService.isValidImage(file.path);
+      if (!validImage) {
+        print('discarded ${basename(sharedImage.path)}: image failed validation - ${invalidImageReason.name}');
+        sharedImage.delete();
+        continue;
+      }
+
+      // copying images to temporary directory folder
+      String newTempPath = '${DirectoryPathProvider.instance.tempDirPath}/${(basename(sharedImage.path).toLowerCase())}';
+      tempImage = await sharedImage.copy(newTempPath);
+      print('saving file: ${basename(tempImage.path)}');
+      sharedFiles.add(tempImage);
+
+      sharedImage.delete();
     }
 
     ReceiveSharingIntent.reset();
 
     return sharedFiles;
   }
-
 }
