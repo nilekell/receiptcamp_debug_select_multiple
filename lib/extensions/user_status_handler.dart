be962605c58742fda5a3387e57f67f5de853fdd5
Nile Kelly <nilekell@gmail.com>
2023-11-01 17:41:35 +0000

    moved BlocProvider to UserStatusHandler extension from main.dart

--- lib/extensions/user_status_handler.dart
+++ lib/extensions/user_status_handler.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:receiptcamp/data/services/purchases.dart';
+import 'package:receiptcamp/logic/cubits/purchases/purchases_cubit.dart';
 import 'package:receiptcamp/presentation/screens/paywall.dart';
 
 extension UserStatusHandler on BuildContext {
@@ -8,15 +10,19 @@ extension UserStatusHandler on BuildContext {
     final PurchasesService purchasesService = PurchasesService.instance;
     purchasesService.checkCustomerPurchaseStatus();
     if (purchasesService.userIsPro) {
+      // callback to whatever pro feature the user was trying to accomplish
       onPro(this);
     } else {
+      // show paywall when user is not a pro user
       Navigator.of(this).pop();
       showModalBottomSheet(
         context: this,
         backgroundColor: Colors.transparent,
         isScrollControlled: true,
         builder: (BuildContext context) {
-          return PaywallView();
+          return BlocProvider(
+        create: ((context) => PurchasesCubit()),
+        child: const PaywallView(),);
         },
       );
     }
