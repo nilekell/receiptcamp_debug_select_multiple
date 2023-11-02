303c6fe6bd45b8fb0f97f1639794cb71b1f12f31
Nile Kelly <nilekell@gmail.com>
2023-11-02 10:00:49 +0000

    PhotoView: adjusted minScale & maxScale

--- lib/presentation/screens/image_view.dart
+++ lib/presentation/screens/image_view.dart
@@ -108,8 +108,8 @@ class _ImageViewScreenState extends State<ImageViewScreen> {
         GestureDetector(
           onTap: _toggleAppBar,
           child: PhotoView(
-            maxScale: PhotoViewComputedScale.contained * 6,
-            minScale: PhotoViewComputedScale.contained * 0.5,
+            maxScale: PhotoViewComputedScale.contained * 3,
+            minScale: PhotoViewComputedScale.contained * 0.8,
             backgroundDecoration: const BoxDecoration(
               color: Colors.black,
             ),
